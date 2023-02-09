691031901f2dfad19444a6449b822db64bd79f29
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2023-02-08 21:26:09 -0500

    a

--- ClassLibraryUnitTest1/ExtensionMethods.cs
+++ ClassLibraryUnitTest1/ExtensionMethods.cs
@@ -6,7 +6,128 @@ using System.Threading.Tasks;
 
 namespace MathLib
 {
-    internal class ExtensionMethods
+    /// <summary>
+    /// An Immutable Fraction
+    /// </summary>
+    public class Fraction
     {
+        // Prefer readonly to const
+        // readonly is a var stored in RAM, but the compiler will not let your code change it (like Java final)
+        private readonly int num, den; // const is replaced at compile time with its value
+
+        public Fraction(int num = 0, int den = 1) // default args
+        {
+            if (den == 0)
+                throw new ArgumentOutOfRangeException("den must not be 0");
+            this.num = num;
+            this.den = den;
+        }
+        //public Fraction(int num) : this(num, 1)
+        //{
+        //}
+        //public Fraction(): this(0)
+        //{
+        //}
+
+        public int Numerator
+        {
+            init // setter for ctor / initlizer 
+            {
+                num = value;
+            }
+            get
+            {
+                return num;
+            }
+
+            
+        }
+        public int Denominator
+        {
+            init
+            {
+                den = value;
+            }
+            get
+            {
+                {
+                    return den;
+                }
+            }
+        }
+        public override bool Equals(Object that)
+        {
+            return this == (Fraction)that;
+        }
+        public static bool operator==(Fraction a, Fraction b)
+        {
+            return a.Denominator * b.Numerator ==
+                   a.Numerator * b.Denominator;
+        }
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return !(a == b);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Numerator,
+                a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator-(Fraction a)
+        {
+            return new Fraction(-a.Numerator,
+                a.Denominator);
+        }
+    }
+
+    // properties are methods disguised as Fields
+    public class Person // trivially simple to store enums in db
+    {
+        private String firstName, lastName;
+        private Gender g;
+
+        public String Initials
+        {
+            get { return "" + FirstName[0] + LastName[0]; }
+        }
+        public String FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value == null || value.Length <= 1)
+                    throw new ArgumentException("Whoah youi have no name?");
+                firstName = value;
+            }
+
+        }
+        public String LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value == null || value.Length <= 1)
+                    throw new ArgumentException("Whoah youi have no name?");
+                lastName = value;
+            }
+
+        }
+    }
+
+    public enum Gender { Unknown, Male, Female } // no enhanced enums
+
+    public static class ExtensionMethods
+    {
+        public static String Clothing(this Gender g)
+        {
+            if (g == Gender.Male)
+            {
+                return "pants";
+            }
+            else return "";
+        }
     }
 }
--- MathLib.Test/UnitTest1.cs
+++ MathLib.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 
 
+using FluentAssertions;
 using Humanizer;
 
 namespace MathLib.Test
@@ -33,14 +34,68 @@ namespace MathLib.Test
         [TestMethod]
         public void ExtTests()
         {
+            String s = ExtensionMethods.Clothing(Gender.Male);
+            Assert.AreEqual("pants", s);
+            Gender.Male.Clothing().Should().Be("pants");
+        }
+
+
+        [TestMethod]
+        public void ExtTests2()
+        {
             String s = Gender.Male.Clothing();
             Assert.AreEqual("pants", s);
+            Gender.Male.Clothing().Should().Be("pants");
         }
-        class Person // trivially simple to store enums in db
+
+        [TestMethod]
+        public void F1()
         {
-            private String firstName, lasteName;
-            private Gender g;
+            var f = new Fraction();
+            Assert.AreEqual(0, f.Numerator);
+            Assert.AreEqual(1, f.Denominator);
         }
+
+        [TestMethod]
+        public void F2()
+        {
+            var f = new Fraction { Numerator = 8 };
+            Assert.AreEqual(8, f.Numerator);
+            Assert.AreEqual(1, f.Denominator);
+        }
+
+        [TestMethod]
+        public void AnonymousType()
+        {
+            var guy = new
+            {
+                Name = "Bob",
+                Age = 23
+            };
+            guy.ToString().Should().Be("{ Name = Bob, Age = 23 }");
+        }
+
+        [TestMethod]
+        public void OperatorOverloading()
+        {
+            Fraction f1 = new Fraction(3, 4),
+                f2 = new Fraction(1, 2);
+
+            var f3 = f1 * f2;
+            f3.Should().Be(new Fraction(3, 8));
+            var f4 = -f1;
+            f4.Should().Be(new Fraction(3, -4));
+        }
+
+        [TestMethod]
+        public void Props()
+        {
+            Person p = new Person(){FirstName = "Bill", LastName = "Gold"};
+            p.FirstName = "Bob";
+            p.LastName = "Smith";
+            p.Initials.Should().Be("BS");
+        }
+
 
         [TestMethod]
         public void EnumLanguageTest()
