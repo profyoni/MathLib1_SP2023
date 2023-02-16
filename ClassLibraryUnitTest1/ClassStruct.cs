79fc449d7a48d22abc5262be12cc209621068d22
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2023-02-15 20:18:09 -0500

    e

--- ClassLibraryUnitTest1/ClassStruct.cs
+++ ClassLibraryUnitTest1/ClassStruct.cs
@@ -18,3 +18,27 @@ public class MyDateTime
 
 // When use structs vs classes
 // small footprint vs large footprint
+
+public class MethodLib2
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public static int SuperSizeMe(ref int x)
+    {
+        x *= 100;
+        return x;
+    }
+    public static MyInt SuperSizeMe(MyInt x)
+    {
+        x.Value *= 100;
+        return x;
+    }
+
+}
+
+public class MyInt
+{
+    public int Value{ get; set; }
+}
--- MathLib.Test/ClassStructTest.cs
+++ MathLib.Test/ClassStructTest.cs
@@ -28,7 +28,53 @@ public class ClassStructTest
         var startOfYear = new DateTime(2023,1,1);
 
         var ts =  (now - startOfYear);
-        ts.Days.Should().Be(43);
+        ts.Days.Should().BeGreaterOrEqualTo(45);
     }
-    
+
+    [TestMethod]
+    public void BasicStruct()
+    {
+        int x=1; //
+        Int32 y=2;
+        x = y;
+        short s;
+        uint unsignedInt, q1=1, q2=2; // 0 -  4B
+        unchecked
+        {
+            (q1 - q2).Should().Be(UInt32.MaxValue);
+        }
+
+        bool b;
+        float f;
+        decimal d; // T-SQL 
+
+        double d1 = 1.0/3, d2=1/3.0;
+        d2 = Math.Sqrt(Math.Sqrt(Math.Sqrt(Math.Sqrt(d2))));
+        d2 = Math.Pow(Math.Pow(Math.Pow(Math.Pow(d2, 2), 2), 2), 2);
+
+        const double Tolerance = 0.000_001; 
+        d1.Should().BeApproximately(d2, Tolerance);
+
+        // check for "equality" for fp
+
+        Assert.IsTrue(Math.Abs(d1-d2) < Tolerance);
+
+        d = 100.18m; // stored precisely
+    }
+
+    [TestMethod]
+    public void MethodLib()
+    {
+        MethodLib2.Add(2,3).Should().Be(5);
+        int a = 1;
+
+        var myInt = new MyInt() { Value = 1 };
+        MethodLib2.SuperSizeMe(myInt);
+        myInt.Value.Should().Be(100);
+
+        MethodLib2.SuperSizeMe(ref a);
+        a.Should().Be(100);
+    }
+
+
 }
